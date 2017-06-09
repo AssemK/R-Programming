8fcc131129288213af39a2394cc5aae2c2651828
AssemK <assem.khussainova@uni-siegen.de>
2017-06-09 16:13:08 +0200

    Add files via upload

---
+++ makeCacheMatrix.R
@@ -0,0 +1,17 @@
+## Functions to cache the matrix inverse
+
+  ## makeCacheMatrix() creates a special matrix object that can cache its inverse
+
+makeCacheMatrix <- function(x=matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
